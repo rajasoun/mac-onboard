1ff6012eb7318cb7ed04171bbae3ecfaa60c35a7
Raja Soundaramourty <rajasoun@cisco.com>
2022-05-05 20:48:38 +0530

    refactor(e2e): edge cases

--- e2e_tests.sh
+++ e2e_tests.sh
@@ -44,79 +44,6 @@ function check() {
     fi
 }
 
-function checkMultiple() {
-    PASSED=0
-    LABEL="$1"
-    echo -e "\n🧪 Testing $LABEL."
-    shift; MINIMUMPASSED=$1
-    shift; EXPRESSION="$1"
-    while [ "$EXPRESSION" != "" ]; do
-        if $EXPRESSION; then ((PASSED++)); fi
-        shift; EXPRESSION=$1
-    done
-    if [ "$PASSED" -ge "$MINIMUMPASSED" ]; then
-        echo "✅ Passed!"
-        return 0
-    else
-        echoStderr "❌ $LABEL check failed."
-        FAILED+=("$LABEL")
-        return 1
-    fi
-}
-
-function checkOSPackages() {
-    LABEL=$1
-    shift
-    echo -e "\n🧪 Testing $LABEL"
-    if brew list --versions go  "$@"; then
-        echo "✅  Passed!"
-        return 0
-    else
-        echoStderr "❌ $LABEL check failed."
-        FAILED+=("$LABEL")
-        return 1
-    fi
-}
-
-function checkExtension() {
-    # Happens asynchronusly, so keep retrying 10 times with an increasing delay
-    EXTN_ID="$1"
-    TIMEOUT_SECONDS="${2:-10}"
-    RETRY_COUNT=0
-    echo -e -n "\n🧪 Looking for extension $1 for maximum of ${TIMEOUT_SECONDS}s"
-    until [ "${RETRY_COUNT}" -eq "${TIMEOUT_SECONDS}" ] || \
-        [ ! -e "$HOME/.vscode-server/extensions/${EXTN_ID}*" ] || \
-        [ ! -e "$HOME/.vscode-server-insiders/extensions/${EXTN_ID}*" ] || \
-        [ ! -e "$HOME/.vscode-test-server/extensions/${EXTN_ID}*" ] || \
-        [ ! -e "$HOME/.vscode-remote/extensions/${EXTN_ID}*" ]
-    do
-        sleep 1s
-        (( RETRY_COUNT++ ))
-        echo -n "."
-    done
-
-    if [ "${RETRY_COUNT}" -lt "${TIMEOUT_SECONDS}" ]; then
-        echo -e "\n✅ Passed!"
-        return 0
-    else
-        echoStderr -e "\n❌ Extension $EXTN_ID not found."
-        FAILED+=("$LABEL")
-        return 1
-    fi
-}
-
-function checkCommon(){
-    PACKAGE_LIST="$(cat packages/brew.txt)"
-
-    # shellcheck disable=SC2086
-    checkOSPackages "common-os-packages" ${PACKAGE_LIST}
-    #checkMultiple "vscode-server" 1 "[ -d $HOME/.vscode-server/bin ]" "[ -d $HOME/.vscode-server-insiders/bin ]" "[ -d $HOME/.vscode-test-server/bin ]" "[ -d $HOME/.vscode-remote/bin ]" "[ -d $HOME/.vscode-remote/bin ]"
-    #check "locale" [ "$(locale -a | grep en_US.utf8)" ]
-    check "sudo" sudo --version
-    check "zsh" zsh --version
-    check "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
-}
-
 function reportResults() {
     failed_results="${#FAILED[@]}"
     if [ "${failed_results}" -ne 0 ]; then
@@ -141,11 +68,21 @@ checkCommon
 
 # Definition specific tests
 #checkExtension "ms-azuretools.vscode-docker"
+
+check "sudo" sudo --version
+check "zsh" zsh --version
+check "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
+check "curl" curl --version
+check "wget" wget --version
+
+check "gh" gh --version
+check "http" http --version
+check "jq" jq --version
+check "netcat" netcat --version
+
 check "aws-vault" aws-vault --version
 check "code" code --version
 check "sentry-cli" sentry-cli --version
-check "gh" gh --version
-check "http" http --version
 
 #check "pre-commit" pre-commit run --all-files
 # Report result
